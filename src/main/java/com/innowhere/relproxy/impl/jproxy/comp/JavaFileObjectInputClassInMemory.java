96eb84c19ca0e442c9f36e3746ad1d9c1b6ac89a
greeneyed <d.lopez.j@gmail.com>
2013-11-13 18:33:32 +0100

    Removed unused imports

--- src/main/java/com/innowhere/relproxy/impl/jproxy/comp/JavaFileObjectInputClassInMemory.java
+++ src/main/java/com/innowhere/relproxy/impl/jproxy/comp/JavaFileObjectInputClassInMemory.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
-import javax.tools.JavaFileObject.Kind;
 import javax.tools.SimpleJavaFileObject;
 
 /**
