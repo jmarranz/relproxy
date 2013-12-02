8644109595778a23d7604a1fccda7eb827bc4675
josemaria.arranz <josemaria.arranz@bqreaders.com>
2013-12-02 22:21:49 +0100

    Minor change script test

--- test_cmd/test_launcher.sh
+++ test_cmd/test_launcher.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+PROJECT=/C/trabajo/empresa/opensource/relproxy_dev
+
 export JAVA_HOME="/C/Archivos de programa/Java/jdk1.7.0_45/"
-export CLASS_PATH=/C/trabajo/empresa/opensource/relproxy_dev/target/classes
-export PATH=$PATH:/C/trabajo/empresa/opensource/relproxy_dev/cmd
+export CLASS_PATH=$PROJECT/target/classes
+export PATH=$PATH:$PROJECT/cmd
 
 ../src/main/webapp/WEB-INF/javashellex/code/test_java_shell
