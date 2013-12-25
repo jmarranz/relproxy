d17116b1892daaa20c3ec754c26319f319ac0b16
josemaria.arranz <josemaria.arranz@bqreaders.com>
2013-12-25 11:51:34 +0100

    Fixed a stupid but big bug

---
+++ test_cmd/setupjava.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo Note: execute this command with source cmd: source setupjava.sh
+
+export JAVA_HOME="/c/Archivos de programa/Java/jdk1.7.0_45/"
+
+echo Defined JAVA_HOME as $JAVA_HOME
